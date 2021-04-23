82c2ad8155dba260432f81999b4ac54904db0de6
Kayla Newlon <kayla.b.newlon@gmail.com>
2021-04-22 17:29:09 -0700

    setting up passport

--- app.js
+++ app.js
@@ -2,29 +2,46 @@ const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv');
 const exphbs = require('express-handlebars')
+const passport = require('passport')
 const morgan = require('morgan')
+const session = require('express-session')
 const connectDB = require('./config/db');
 
 //Load config
-dotenv.config({ path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB();
 
 const app = express();
 
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
 //Handlebars Middleware
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
+
+
+// Sessions Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Passport Middleware
+app.use(passport.initialize)
+app.use(passport.session)
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
-app.use('/', require('./routes/index') )
+app.use('/', require('./routes/index'))
 
 
 const PORT = process.env.PORT || 3000
