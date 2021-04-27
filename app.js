6f1b563b2dc0dffd3bc8697ba0544383323652cc
Kayla Newlon <kayla.b.newlon@gmail.com>
2021-04-26 21:40:39 -0700

    edit story running

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const morgan = require('morgan');
 const session = require('express-session');
@@ -28,36 +29,57 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method
+            delete req.body._method
+            return method
+        }
+    })
+)
+
+
 //Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
 // Handlebars Helpers
-const { formatDate, truncate, stripTags, editIcon, select  } = require('./helpers/hbs')
+const {
+    formatDate,
+    truncate,
+    stripTags,
+    editIcon,
+    select
+} = require('./helpers/hbs')
 
 //Handlebars Middleware
-app.engine('.hbs', exphbs({ helpers:{ 
-    formatDate,
-    truncate, 
-    stripTags, 
-    editIcon, 
-    select
-},
-defaultLayout: 'main', extname: '.hbs' }));
+app.engine('.hbs', exphbs({
+    helpers: {
+        formatDate,
+        truncate,
+        stripTags,
+        editIcon,
+        select
+    },
+    defaultLayout: 'main', extname: '.hbs'
+}));
 app.set('view engine', '.hbs');
 
 
 // Sessions Middleware
 app.use(
-  session({
-      secret: 'story book',
-      resave: false,
-      saveUninitialized: false,
-      store: MongoDbStore.create({
-          mongoUrl: process.env.MONGO_URI
-      })
-  })
+    session({
+        secret: 'story book',
+        resave: false,
+        saveUninitialized: false,
+        store: MongoDbStore.create({
+            mongoUrl: process.env.MONGO_URI
+        })
+    })
 );
 
 
@@ -66,7 +88,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 //Set global variable to access user within stories template
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.locals.user = req.user || null
     next()
 })
