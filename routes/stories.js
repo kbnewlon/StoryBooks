3ba42ccf12e6e83f04be232766810640511b3f5a
Kayla Newlon <kayla.b.newlon@gmail.com>
2021-04-26 22:03:37 -0700

    show and edit user stories

--- routes/stories.js
+++ routes/stories.js
@@ -62,7 +62,6 @@ router.get('/:id', ensureAuth, async (req, res) => {
 })
 
 
-
 //Show edit page
 //Route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -117,6 +116,27 @@ router.delete('/:id', ensureAuth, async (req, res) => {
     try {
         await Story.remove({ _id: req.params.id })
         res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+
+
+//Show User stories
+//Route GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public'
+        })
+        .populate('user')
+        .lean()
+
+        res.render('stories/index', {
+            stories
+        })
     } catch (err) {
         console.error(err)
         return res.render('error/500')
