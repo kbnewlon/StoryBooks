48d7d46ee4a87f37e9a87ad9380e905f738f0e97
Kayla Newlon <kayla.b.newlon@gmail.com>
2021-04-26 17:28:43 -0700

    error renders

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,31 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+const Story = require('../models/Story')
+
 //Login/Landing page
 //Route GET /
-router.get('/', ensureGuest, (req,res)=>{
-    res.render('login',{
-        layout:'login',
+router.get('/', ensureGuest, (req, res) => {
+    res.render('login', {
+        layout: 'login',
     })
 })
 
 //Dashboard
 //Route Get /dashboard
-router.get('/dashboard', ensureAuth, (req,res)=>{
-    console.log(req.user)
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        console.log(req.user)
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+
 })
 
 
